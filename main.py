ee5180be77f864a854e56d8a7b859faf23b5470a
[Amina CHABLOU] <[minachablou@gmail.com]>
2025-10-04 22:14:04 +0100

    Feat: Ajout d'une nouvelle fonctionnalité d'affichage

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # main.py
 print("Version initiale du projet par le Scrum Master")
+
+def nouvelle_fonctionnalite():
+    print("Ceci est une nouvelle fonctionnalité ajoutée par le développeur !")
+
+nouvelle_fonctionnalite()
